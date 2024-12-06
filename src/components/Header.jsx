2b9342fb41eee8b3db3e129dcb235be9a3639c85
desarrollador software <desarrollo@merkahorrosas.com>
2024-12-06 13:03:01 -0500

    subimos carpetas del proyecto

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Header =() => {
+    return(
+        <header>
+            
+            <Link to="/"></Link>
+            <Link to="/contribuciones">contribuciones</Link>
+        </header>
+    )
+}
+
+export{ Header};
