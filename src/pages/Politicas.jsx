111c86dd761af35ca8a347f266089431f6ef09ef
Juan Manuel <juanmerkahorro@gmail.com>
2024-12-09 15:42:45 -0500

    avanzamos migracion del home a react

---
+++ src/pages/Politicas.jsx
@@ -0,0 +1,111 @@
+import './Politicas.css'
+import { Footer } from '../components/Footer.jsx';
+
+const Politicas = () => {
+    return (
+        <div className="container">
+        <h1>Política de Privacidad</h1>
+      <p><strong>Última actualización: 17/09/2024</strong></p>
+
+      <p>
+        Esta Política de Privacidad describe nuestras políticas y procedimientos sobre la recopilación, uso y divulgación de su información cuando utiliza el Servicio y le informa sobre sus derechos de privacidad y cómo la ley lo protege.
+      </p>
+
+      <h2>Interpretación y Definiciones</h2>
+
+      <h3>Interpretación</h3>
+      <p>
+        Las palabras con la primera letra en mayúscula tienen significados definidos bajo las siguientes condiciones. Las siguientes definiciones tendrán el mismo significado sin importar si aparecen en singular o en plural.
+      </p>
+
+      <h3>Definiciones</h3>
+      <p>Para los fines de esta Política de Privacidad:</p>
+      <ul>
+        <li><strong>Cuenta:</strong> significa una cuenta única creada para usted para acceder a nuestro Servicio o partes de nuestro Servicio.</li>
+        <li><strong>Empresa:</strong> se refiere a Merkahorro S.A.S.</li>
+        <li><strong>País:</strong> se refiere a: Colombia.</li>
+        <li><strong>Dispositivo:</strong> significa cualquier dispositivo que pueda acceder al Servicio, como una computadora, un teléfono móvil o una tableta digital.</li>
+        <li><strong>Datos Personales:</strong> son cualquier información que se relacione con una persona identificada o identificable.</li>
+        <li><strong>Servicio:</strong> se refiere al Sitio Web.</li>
+        <li><strong>Proveedor de Servicios:</strong> significa cualquier persona natural o jurídica que procese los datos en nombre de la Empresa.</li>
+        <li><strong>Servicio de Redes Sociales de Terceros:</strong> se refiere a cualquier sitio web o sitio de redes sociales a través del cual un Usuario puede iniciar sesión o crear una cuenta para utilizar el Servicio.</li>
+        <li><strong>Datos de Uso:</strong> se refiere a datos recopilados automáticamente, ya sea generados por el uso del Servicio o por la infraestructura del Servicio mismo.</li>
+        <li><strong>Sitio Web:</strong> se refiere a merkahorro.com, accesible desde https://www.merkahorro.com/.</li>
+        <li><strong>Usted:</strong> significa la persona que accede o utiliza el Servicio, o la empresa u otra entidad legal en nombre de la cual dicha persona está accediendo o utilizando el Servicio, según corresponda.</li>
+      </ul>
+
+      <h2>Recopilación y Uso de sus Datos Personales</h2>
+
+      <h3>Tipos de Datos Recopilados</h3>
+
+      <h4>Datos Personales</h4>
+      <p>
+        Mientras utiliza nuestro Servicio, podemos pedirle que nos proporcione cierta información personal identificable que puede ser utilizada para contactarlo o identificarlo. La información personal identificable puede incluir, pero no se limita a:
+      </p>
+      <ul>
+        <li>Dirección de correo electrónico</li>
+        <li>Nombre y apellido</li>
+        <li>Número de teléfono</li>
+        <li>Dirección, Estado, Provincia, Código Postal, Ciudad</li>
+      </ul>
+
+      <h4>Datos de Uso</h4>
+      <p>
+        Los Datos de Uso se recopilan automáticamente al usar el Servicio. Los Datos de Uso pueden incluir información como la dirección IP de su dispositivo, tipo de navegador, versión del navegador, las páginas de nuestro Servicio que visita, la hora y fecha de su visita, el tiempo pasado en esas páginas, identificadores de dispositivo únicos y otros datos de diagnóstico.
+      </p>
+
+      <h4>Información de Servicios de Redes Sociales de Terceros</h4>
+      <p>
+        La Empresa le permite crear una cuenta e iniciar sesión para usar el Servicio a través de los siguientes Servicios de Redes Sociales de Terceros: Cuenta de Microsoft. Si decide registrarse a través de una cuenta de Microsoft o de otro Servicio de Redes Sociales de Terceros, podemos recopilar Datos Personales que ya están asociados con su cuenta del Servicio de Redes Sociales de Terceros.
+      </p>
+
+      <h2>Uso de sus Datos Personales</h2>
+      <p>La Empresa utiliza los Datos Personales para los siguientes fines:</p>
+      <ul>
+        <li>Para proporcionar y mantener nuestro Servicio</li>
+        <li>Para gestionar su Cuenta</li>
+        <li>Para la ejecución de un contrato</li>
+        <li>Para contactarlo</li>
+        <li>Para proporcionarle noticias y ofertas especiales</li>
+      </ul>
+
+      <h2>Derechos de los Titulares de Datos Personales</h2>
+      <p>
+        De acuerdo con la Ley 1581 de 2012, como titular de sus datos personales, usted tiene derecho a:
+      </p>
+      <ul>
+        <li>Acceder: a sus datos personales bajo nuestro control.</li>
+        <li>Actualizar y Rectificar: sus datos personales cuando sean incorrectos, incompletos o estén desactualizados.</li>
+        <li>Solicitar la Eliminación: de sus datos personales cuando no se requieran para la finalidad para la cual fueron recopilados.</li>
+        <li>Revocar su Autorización: para el tratamiento de sus datos personales en cualquier momento.</li>
+        <li>Presentar Quejas ante la Autoridad de Protección de Datos.</li>
+      </ul>
+
+      <h2>Seguridad de sus Datos Personales</h2>
+      <p>
+        La seguridad de sus Datos Personales es importante para nosotros, pero recuerde que ningún método de transmisión a través de Internet o método de almacenamiento electrónico es 100% seguro. Mientras nos esforzamos por usar medios comercialmente aceptables para proteger sus Datos Personales, no podemos garantizar su seguridad absoluta.
+      </p>
+
+      <h2>Enlaces a Otros Sitios Web</h2>
+      <p>
+        Nuestro Servicio puede contener enlaces a otros sitios web que no son operados por nosotros. Si hace clic en un enlace de terceros, será dirigido al sitio web de ese tercero. Le recomendamos encarecidamente que revise la Política de Privacidad de cada sitio web que visite.
+      </p>
+
+      <h2>Cambio en esta Política de Privacidad</h2>
+      <p>
+        Podemos actualizar nuestra Política de Privacidad de vez en cuando. Le notificaremos cualquier cambio publicando la nueva Política de Privacidad en esta página.
+      </p>
+
+      <h2>Contáctenos</h2>
+      <p>Si tiene alguna pregunta sobre esta Política de Privacidad, por favor contáctenos:</p>
+      <ul>
+        <li>Por correo electrónico: paginaweb@merkahorrosas.com</li>
+        <li>Por teléfono: 324 5597862</li>
+      </ul>
+      <Footer/>
+    </div>
+  );
+};
+    
+
+export {Politicas}
