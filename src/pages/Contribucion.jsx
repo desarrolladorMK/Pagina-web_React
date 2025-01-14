ab8739ff6d2ca5782abd220c2e03bd1135b8f39b
Juan Manuel <juanmerkahorro@gmail.com>
2025-01-14 14:28:21 -0500

    nuevo diseño videos

--- src/pages/Contribucion.jsx
+++ src/pages/Contribucion.jsx
@@ -1,85 +1,143 @@
 import React from 'react';
 import './Contribucion.css';
 import { Footer } from '../components/Footer';
-import {ChatBot} from '../components/ChatBot';
+import { ChatBot } from '../components/ChatBot';
 
 const Contribucion = () => {
+
+  // Datos de las secciones con las especificaciones proporcionadas
   const sections = [
+    // Obras Sociales
     {
       title: 'Obras Sociales',
       videos: [
-        'https://www.youtube.com/embed/sz58ZOmlPD0',
-        'https://www.youtube.com/embed/vkFFss9pUO8',
-        'https://www.youtube.com/embed/zdRT0aZXXDc',
-        'https://www.youtube.com/embed/MZiyMwtGJaY',
-      ],
+        {
+          thumbnail: 'obras1.jpg', // Reemplazar por la URL de la imagen
+          instagramLink: 'https://www.instagram.com/reel/C7kniH5MzdD/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==' // Reemplazar por la URL de la publicación de Instagram
+        },
+        {
+          thumbnail: 'obras2.jpg', // Reemplazar por la URL de la imagen
+          instagramLink: 'https://www.instagram.com/reel/C3Q5KrqPTTu/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==' // Reemplazar por la URL de la publicación de Instagram
+        },
+        {
+          thumbnail: 'obras3.jpg', // Reemplazar por la URL de la imagen
+          instagramLink: 'https://www.instagram.com/reel/CyqyuXZOhJP/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==' // Reemplazar por la URL de la publicación de Instagram
+        },
+        {
+          thumbnail: 'obras4.jpg', // Reemplazar por la URL de la imagen
+          instagramLink: 'https://www.instagram.com/reel/C6cH2SxPDLS/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==' // Reemplazar por la URL de la publicación de Instagram
+        }
+      ]
     },
+
+    // Merkahorro Contribuye
     {
-      title: 'Merkarhorro Contribuye',
+      title: 'Merkahorro Contribuye',
       videos: [
-        'https://www.youtube.com/embed/tSOY8iG4uOk',
-        'https://www.youtube.com/embed/9qb61_Z1_CI',
-        'https://www.youtube.com/embed/xyOIH0lTjCw',
-        'https://www.youtube.com/embed/PAWmUlHRBEA',
-        'https://www.youtube.com/embed/QP6VO9lKKfo',
-        'https://www.youtube.com/embed/fAWbs2tGL7Q',
-        'https://www.youtube.com/embed/OMuKrC_HGBQ',
-        'https://www.youtube.com/embed/OoLIKaPO-FM',
-        'https://www.youtube.com/embed/LnSgVyBf9Zw',
-      ],
+        {
+          thumbnail: 'contribuye1.jpg',
+          instagramLink: 'https://www.instagram.com/reel/C4x3VVRO9qv/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=='
+        },
+        {
+          thumbnail: 'contribuye2.jpg',
+          instagramLink: 'https://www.instagram.com/reel/C_nYZ3cuHAd/?utm_source=ig_web_button_share_sheet&igsh=MzRlODBiNWFlZA=='
+        },
+        {
+          thumbnail: 'contribuye3.jpg',
+          instagramLink: 'https://www.instagram.com/reel/C8u13V7PThy/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=='
+        },
+        {
+          thumbnail: 'contribuye4.jpg',
+          instagramLink: 'https://www.instagram.com/reel/C_jViLkPKUc/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=='
+        },
+        {
+          thumbnail: 'contribuye5.jpg',
+          instagramLink: 'https://www.instagram.com/reel/C_0vYl5vqAp/?igsh=d2o4dTl3czMwajlu'
+        },
+        {
+          thumbnail: 'contribuye6.jpg',
+          instagramLink: 'https://www.instagram.com/reel/C_k8OLQOgJG/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=='
+        },
+        {
+          thumbnail: 'contribuye7.jpg',
+          instagramLink: 'https://www.instagram.com/reel/C9-NIlGpt5Q/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=='
+        },
+        {
+          thumbnail: 'contribuye8.jpg',
+          instagramLink: 'https://www.instagram.com/reel/C9LCH0_vI6l/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=='
+        },
+        {
+          thumbnail: 'contribuye9.jpg',
+          instagramLink: 'https://www.instagram.com/reel/C9f8dyivkiw/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=='
+        }
+      ]
     },
+
+    // Bachilleres Merkahorro
     {
       title: 'Bachilleres Merkahorro',
       videos: [
-        'https://www.youtube.com/embed/7eJUrwJyIYo',
-        'https://www.youtube.com/embed/Qkw4bek6nZE',
-        'https://www.youtube.com/embed/B-MmXCCilGA',
-      ],
+        {
+          thumbnail: 'bachiller1.jpg',
+          instagramLink: 'https://www.instagram.com/reel/CuLBPv_tGGj/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=='
+        },
+        {
+          thumbnail: 'bachiller2.jpg',
+          instagramLink: 'https://www.instagram.com/reel/C7ZMvL6MRlI/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=='
+        },
+        {
+          thumbnail: 'bachiller3.jpg',
+          instagramLink: 'https://www.instagram.com/reel/DAYgYCjuZN6/?igsh=MTBrcjNoemFlOTFzcg=='
+        }
+      ]
     },
+
+    // Escuela Aristotélica
     {
       title: 'Escuela Aristotélica',
       videos: [
-        'https://www.youtube.com/embed/ew554uDvcXI',
-        'https://www.youtube.com/embed/ZXpE-qY0tlY',
-      ],
+        {
+          thumbnail: 'aristotelica1.jpg',
+          instagramLink: 'https://www.instagram.com/reel/C-TAH8FP0G6/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=='
+        },
+        {
+          thumbnail: 'aristotelica2.jpg',
+          instagramLink: 'https://www.instagram.com/reel/DAuQga9PVmt/?igsh=ZDBzYmMxbHd5a2lh'
+        }
+      ]
     },
+
+    // Club Amas de Casa
     {
       title: 'Club Amas de Casa',
       videos: [
-        'https://www.youtube.com/embed/nlbdeQaB1PE',
-        'https://www.youtube.com/embed/_PajpyFOwLo',
-        'https://www.youtube.com/embed/NEsLgp-5Udo',
-        'https://www.youtube.com/embed/sNQ8G34DIXE',
-        'https://www.youtube.com/embed/8M1WstwXi0s',
-        'https://www.youtube.com/embed/JPxmWdMQfiU',
-      ],
-    },
-  ];
-  
-  const renderVideo = (video) => {
-    if (video.includes('youtube.com')) {
-      const videoId = video.split('/').pop().split('?')[0]; // Extraer el ID del video
-      return (
-        <iframe
-          key={video}
-          className="video-item youtube-video"
-          src={`https://www.youtube.com/embed/${videoId}`}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-          loading="lazy"
-        ></iframe>
-      );
-    } else {
-      return (
-        <video key={video} className="video-item" controls>
-          <source src={video} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      );
+        {
+          thumbnail: 'club1.jpg',
+          instagramLink: 'https://www.instagram.com/reel/C5bV2PrrAmo/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=='
+        },
+        {
+          thumbnail: 'club2.jpg',
+          instagramLink: 'https://www.instagram.com/reel/C__PUCQPtZM/?igsh=NHRiOG9sbTBxYXQ3'
+        },
+        {
+          thumbnail: 'club3.jpg',
+          instagramLink: 'https://www.instagram.com/reel/C4BONVGraG4/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=='
+        },
+        {
+          thumbnail: 'club4.jpg',
+          instagramLink: 'https://www.instagram.com/reel/C-TcFsKvWoh/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=='
+        },
+        {
+          thumbnail: 'club5.jpg',
+          instagramLink: 'https://www.instagram.com/reel/DA89lI-PNUO/?igsh=MTBwdnprdThoY3E4cQ=='
+        },
+        {
+          thumbnail: 'club6.jpg',
+          instagramLink: 'https://www.instagram.com/reel/C7pGU1eJJe_/?igsh=MW41c2JqMnZxajBjZQ=='
+        }
+      ]
     }
-  };
+  ];
 
   return (
     <div className="contribucion-body">
@@ -94,9 +152,17 @@ const Contribucion = () => {
       <main>
         {sections.map((section, index) => (
           <section key={index} className="contribucion-section">
-            <h2>{section.title}</h2>
+            <h2 className="section-title">{section.title}</h2>
             <div className="contribucion-video-container">
-              {section.videos.map((video) => renderVideo(video))}
+              {section.videos.map((video, videoIndex) => (
+                <a href={video.instagramLink} key={videoIndex} target="_blank" rel="noopener noreferrer">
+                  <img
+                    src={video.thumbnail}
+                    alt={`Thumbnail de ${section.title}`}
+                    className="contribucion-thumbnail"
+                  />
+                </a>
+              ))}
             </div>
           </section>
         ))}
